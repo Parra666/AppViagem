a05f73c3dce446db2aa91e6374fd1cdede810827
Usuario <Usuario@CPD02-M18>
2022-09-06 11:44:11 -0300

    Adicionar arquivos de projeto.

---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using App1.Model;
+using App1.View;
+using Xamarin.Forms;
+
+using System.Globalization;
+using System.Threading;
+using System.Collections.ObjectModel;
+
+namespace App1
+{
+    public partial class App : Application
+    {
+        public static ObservableCollection<Pedagio> ListaPedagios = new ObservableCollection<Pedagio>();
+
+        public App()
+        {
+            InitializeComponent();
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+            MainPage = new NavigationPage(new DadosViagem());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App1/App1/View/Listagem.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1.Model;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Listagem : ContentPage
+    {
+        public Listagem()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Formulario());
+        }
+
+        protected override void OnAppearing()
+        {
+            try
+            {
+                lst_pedagios.ItemsSource = App.ListaPedagios;
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            MenuItem disparador = (MenuItem)sender;
+
+            Pedagio pedagio_selecionado = (Pedagio)disparador.BindingContext;
+
+            bool confirmacao = await DisplayAlert("Tem certeza?",
+                                                  "Excluir pedágio " + pedagio_selecionado.Localizacao + "?",
+                                                  "Sim", "Não");
+            if (confirmacao)
+            {
+                App.ListaPedagios.Remove(pedagio_selecionado);
+
+        
+                lst_pedagios.ItemsSource = App.ListaPedagios;
+            }
+        }
+    }
+}
